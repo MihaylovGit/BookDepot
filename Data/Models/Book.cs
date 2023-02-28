c8dbb5237f7bcedf384864e6cfb7fa258952a2e0
MihaylovGit <kostadin9009@students.softuni.bg>
2023-02-28 13:24:29 +0200

    Add Book and Author entity

---
+++ Data/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿namespace BookDepot.Data.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public Author Author { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public DateTime PublishedOn { get; set; }
+
+    public int Pages { get; set; }
+
+    public string Language { get; set; } = null!;
+
+    public string ISBN { get; set; } = null!;
+}
